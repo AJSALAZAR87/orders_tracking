3bacac43bc515fc4e83d0421c7c0d176ac986504
herr_asalazar@hotmail.com <herr_asalazar@hotmail.com>
2025-01-15 22:54:49 -0600

    feat: add CORS module

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const twilio = require('twilio');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
 const logger = require('./src/utils/logger')
 const routes = require('./src/routes/routes');
 const logRequest = require('./src/middlewares/logRequest');
-const dotenv = require('dotenv');
 
 const pool = require('./src/config/database')
 
@@ -19,6 +21,22 @@ pool.connect()
   .catch(err => {
     console.error('Connection to PostgreSQL database failed:', err);
   });
+
+// const allowedOrigin = 'http://localhost:3000';
+
+// // Configure CORS options
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//       if (!origin || origin === allowedOrigin) {
+//           callback(null, true); // Allow the request
+//       } else {
+//           callback(new Error('Not allowed by CORS')); // Block the request
+//       }
+//   },
+// };
+
+// Apply CORS middleware to all routes
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
