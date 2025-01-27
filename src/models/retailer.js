81213e5a9579e90bcf366e87ba6b0931e68fee70
herr_asalazar@hotmail.com <herr_asalazar@hotmail.com>
2025-01-27 14:22:16 -0600

    feat: add retailers pagination and search filter

--- src/models/retailer.js
+++ src/models/retailer.js
@@ -1,9 +1,19 @@
 const pool = require('../config/database');
 const logger = require('../utils/logger');
 
-const getAllRetailers = async () => {
+const getAllRetailers = async (req) => {
   try {
-    const query = `
+    const page = parseInt(req.query.page) || 1;  
+    const limit = parseInt(req.query.limit) || 10;
+    const search = req.query.search || '';
+    
+    if (limit > 100) limit = 100; 
+    if (page < 1) page = 1;
+    const offset = (page - 1) * limit;
+    const sort = req.query.sort || 'ASC';
+
+    let countQuery = 'SELECT COUNT(*) FROM public.retailers';
+    let query1 = `
       SELECT 
         r.id AS retailer_id,
         r.name AS retailer_name,
@@ -34,11 +44,44 @@ const getAllRetailers = async () => {
           retailers r
       LEFT JOIN 
           cases ca ON ca.retailer_id = r.id
-      GROUP BY 
-          r.id;
     `;
-    const result = await pool.query(query);
-    return result.rows;
+
+    const queryParams = [];
+    let countResult;
+    let total;
+
+    if(search) {
+      countQuery += ` WHERE (name ILIKE $${queryParams.length + 1} OR email ILIKE $${queryParams.length + 1})`;
+      query1 += ` WHERE (r.name ILIKE $${queryParams.length + 1} OR r.email ILIKE $${queryParams.length + 1})`;
+      queryParams.push(`%${search}%`);
+    }
+
+    query1 += ` GROUP BY r.id 
+                ORDER BY r.id ${sort} 
+                LIMIT $${queryParams.length + 1} 
+                OFFSET $${queryParams.length + 2} 
+                ;`;
+    queryParams.push(limit, offset);
+
+    countResult = await pool.query(countQuery, queryParams.slice(0, queryParams.length - 2));
+    total = parseInt(countResult.rows[0].count);
+
+    const result = await pool.query(query1, queryParams);
+    logger.info(`Retrieved ${result.rows.length} couriers`);
+    
+    const totalPages = Math.ceil(total / limit);
+    const pagination = {
+      totalItems: total,
+      totalPages: totalPages,
+      currentPage: page,
+      pageSize: limit,
+    };
+
+    const result2 = {
+      data: result.rows,
+      pagination
+    }
+    return result2;
   } catch (err) {
     logger.error(`Error in getAllRetailers: ${err.message}`);
     throw new Error(`Error retrieving all Retailers: ${err.message}`);
